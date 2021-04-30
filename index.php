f2d6a59962a371657a5e9e5bcaad61c058c3691c
unknown <tsiky>
2021-04-30 16:05:22 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>Accueil</title>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/animation.css" media="all"/>
+</head>
+<body>
+   
+  <div class="cube"></div>
+  <div class="cube"></div>
+  <div class="cube"></div>
+  <div class="cube"></div>
+  <div class="cube"></div>
+
+<header>
+  <section class="header-content">
+    <h1><font color="white">BIENVENU DANS CE GESTION D'UTILISATEUR</font></h1></br>
+    <p><span class="typed-text"></span><span class="cursor">&nbsp;</span></p></br>
+    <form action="authentification.php">
+      <button>CONSULTER LES UTILISATEURS</button>
+    </form>
+  </section>
+</header>
+
+<script src="js/tsikytext.js"></script>
+
+</body>
+</html>
